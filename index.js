597663f4d0054bd947346196d2c18bedd0bbad51
Xoji01 <135627056+Xoji01@users.noreply.github.com>
2023-09-26 14:58:22 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,18 +3,7 @@ let total = arr[0][0].a.price+arr[1][0].a.price+arr[2][0].a.price
 console.log(
     total
 )
-// let qq = +prompt('Какое число')
-// let i = 0
-// do{
-//     if(qq <100){
-//       qq=+prompt(`Какое число`);
-//     }
-//     else(){
-//       alert('Добро пожаловать')
-//       }
-//     i--
-//   }
-//   while(i>0 ) 
+
 let agg  = [4, 16, 19, 22, 11, 144, 967, 19124]
 let even = []
 let odd = []
